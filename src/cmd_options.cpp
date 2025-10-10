c2f7c42470994ead5fac24cba61ba72a7e0a77d3
Alex <kelheor23@gmail.com>
2025-10-10 14:52:22 +0300

    options fix

--- src/cmd_options.cpp
+++ src/cmd_options.cpp
@@ -9,9 +9,9 @@ ProgramOptions::ProgramOptions() : desc_("Allowed options") {
     desc_.add_options()
         ("help,h", "Show help")
         ("command,c",   po::value<COMMAND_TYPE>(&command_)->required(),     "Select command: encrypt, decrypt, checksum")
-        ("input,i",     po::value<std::string>(&inputFile_)->required(),    "Path to the input file")
-        ("output,o",    po::value<std::string>(&outputFile_)->required(),   "Path to the output file")
-        ("password,p",  po::value<std::string>(&password_)->required(),     "Password for encryption and decryption");
+        ("input,i",     po::value<std::string>(&inputFile_),    "Path to the input file")
+        ("output,o",    po::value<std::string>(&outputFile_),   "Path to the output file")
+        ("password,p",  po::value<std::string>(&password_),     "Password for encryption and decryption");
 }
 
 ProgramOptions::~ProgramOptions() = default;
@@ -23,9 +23,28 @@ void ProgramOptions::Parse(int argc, char *argv[]) {
         if (vm_.count("help")) {            
             helpRequested_ = true;
             return;
-        }
-
+        }       
         po::notify(vm_);
+
+        switch (command_) {
+        case COMMAND_TYPE::ENCRYPT:
+        case COMMAND_TYPE::DECRYPT:
+            if (inputFile_.empty())
+                throw std::runtime_error{std::format("Error parsing command line: inputFile_ is empty")};
+            if (outputFile_.empty())
+                throw std::runtime_error{std::format("Error parsing command line: outputFile_ is empty")};
+            if (password_.empty())
+                throw std::runtime_error{std::format("Error parsing command line: password_ is empty")};
+            break;
+
+        case COMMAND_TYPE::CHECKSUM: 
+            if (inputFile_.empty())
+                throw std::runtime_error{std::format("Error parsing command line: inputFile_ is empty")};
+            break;
+        
+        default:
+            break;
+        }                
     } 
     catch (const po::error &e) {
         throw std::runtime_error{std::format("Error parsing command line: {}\n", e.what())};
